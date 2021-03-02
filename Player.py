6cbfcb69cca04c2bfb63cb2a80c57c0dc1a67fae
tony_sax <tony@wantenna.com>
2021-03-01 20:20:17 -0600

    Master: Adding maritime trade for trade phase.

--- Player.py
+++ Player.py
@@ -8,6 +8,7 @@ class Player:
         self.color = color
         self.my_turn = False
         self.resources = {"Lumber":0,"Grain":0,"Wool":0,"Brick":0,"Ore":0}
+        self.maritime_trade_ratio = {"Lumber":4,"Grain":4,"Wool":4,"Brick":4,"Ore":4}
         self.settlements = 0    # max=5
         self.cities = 0         # max=4
         self.roads = 0          # max=15
@@ -16,3 +17,64 @@ class Player:
         self.has_longest_road = false
         self.has_largest_army = false
         self.victory_points = 0 # cities & settlements count towards victory points
+
+
+    def TradePhase(self):
+        still_trading = True
+        maritime_trading_possible = False
+
+        while (still_trading):
+            # Domestic
+            print("Domestic Trade? (Y/N)")
+            domestic_confirmation = input()
+            if domestic_confirmation == "Y":
+                DomesticTrade()
+
+            # Maritime
+            print("Maritime Trade? (Y/N)")
+            maritime_confirmation = input()
+            if maritime_confirmation == "Y":
+                MaritimeTrade()
+
+            print("Are you finished trading? Y/N")
+            input_trading = input()
+            if input_trading == "Y":
+                still_trading = True
+            else:
+                still_trading = False
+
+    # Domestic Trade is trading with the other players
+    def DomesticTrade(self):
+
+    # Maritime Trade is trading with nobody/the bank.
+    def MaritimeTrade(self):
+        print("Listing trade for each available resource...")
+        # List the best available trade for each card.
+        for key, value in self.maritime_trade_ratio:
+            print(self.maritime_trade_ratio.keys().index(key)+1, ". ", key, ":", value, "to 1 trade")
+        print("Select resource you want (Input resource name): ")
+        resource_wanted = input()
+        resource_wanted = resource_wanted.title()
+
+        print("Select what resource you will give up for it (Input resource name): ")
+        # List what cards you can trade for, and how much you have of that card.
+        for key, value in self.resources:
+            print(self.resources.keys().index(key)+1, ".", key, ": ", "You have ", value, " currently.")
+        resource_given = input()
+        resource_given = resource_given.title()
+
+        # If the players has enough cards to give away for their chosen resource
+        if self.resources[resource_given] >= self.maritime_trade_ratio[resource_given]:
+            print("You are trading ", self.maritime_trade_ratio[resource_given], " ", resource_given, " for 1 ", resource_wanted, ".")
+            print("Is that OK? (Y/N)")
+            trade_confirmation = input()
+            if trade_confirmation == "Y":
+                # Finish the trade
+                self.resources[resource_wanted] = self.resources[resource_wanted] + 1
+                self.resources[resource_given] = self.resources[resource_given] - self.maritime_trade_ratio[resource_given]
+            elif trade_confirmation == "N":
+                # Cancel the trade
+                print("You canceled the trade.")
+        # The player does not have enough cards
+        else:
+            print("You did not have enough ", resource_given, ".")
